5247d2352c2760ec4e3b2586b007df99948e7b47
Karam El-loh <karamjoudah@gmail.com>
2018-12-28 15:07:42 -0500

    v2.9
    -Updated credits, standing, skills, and tools and technologies.
    - Added senior design project to portfolio

---
+++ js/date.js
@@ -0,0 +1,10 @@
+
+
+window.addEventListener('load',function(){
+   
+    var thisDate=document.getElementById('date');
+    var d=new Date();
+    
+    thisDate.innerHTML=d.getFullYear();
+    
+});
